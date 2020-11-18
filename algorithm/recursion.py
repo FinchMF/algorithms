f4419930c78324bba6ce8575e4fae40a340fbbab
Matthew Finch <finchrmatthew@gmail.com>
2020-11-17 20:09:04 -0500

    factorial

--- algorithm/recursion.py
+++ algorithm/recursion.py
@@ -2,10 +2,14 @@
 def factorial(n):
 
     if n == 0:  
-        print(n)
         return 1
 
     else: 
-        print(n)
         return n * factorial(n-1)
 
+
+if __name__ == '__main__':
+
+    num = [0, 6, 7, 10, 333]
+    for n in num:
+        print(f"The factorial of {n} is {factorial(n)}")
